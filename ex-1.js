d30e0a4df36d5e1616fadda73eff881c4352a8b4
Pom <chayakon500@gmail.com>
2024-08-18 13:19:39 +0700

    BP5-POM-Ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,12 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18 = true;
+let hasCriminalBlacklist = true;
 
-let isAllow;
+isOver18 = true
+hasCriminalBlacklist = false
+
+let isAllow = isOver18 && !hasCriminalBlacklist;
 
 // Start coding here
+
 
 console.log(isAllow);
